043a611191a514ca41c08f8dfc3eea07c8402ecf
temasaguru <temasaguru@gmail.com>
2022-12-09 01:50:21 +0900

    fix: spotifyアルバムジャケットの画像最適化を廃止
    close #16

--- drivers/views/components/layout/DefaultFooter.tsx
+++ drivers/views/components/layout/DefaultFooter.tsx
@@ -3,7 +3,7 @@ const DefaultFooter = () => {
     <footer className="flex flex-col justify-center gap-y-3 p-8 text-center">
       <div>(c) 2022 temasaguru</div>
       <div className="text-sm">
-        {`当サイトに掲載しているアルバムアートワークは著作権法上の範囲でSpotifyから引用しており、楽曲の権利は提供元に帰属します。`}
+        {`当サイトに掲載しているアルバムアートワークはSpotify APIで取得したURLを使って表示しており、公式やDiscordの埋め込み機能と同様にSpotifyサーバーの画像をそのまま埋め込んでいるため、権利上の問題はないと判断し運用しています。楽曲の権利は提供元に帰属します。`}
         <br className="hidden xl:block" />
         {`当サイトはいかなる広告・解析サービスも使用しておらず、管理画面以外でCookieは使用しておりません。`}
       </div>
